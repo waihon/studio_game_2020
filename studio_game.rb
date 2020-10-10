7b168153347bfb364554c33009a5e987612a4f6f
Waihon Yew <yewwaihon@gmail.com>
2020-10-10 11:52:04 +0800

    Define the object's behavior

--- studio_game.rb
+++ studio_game.rb
@@ -2,8 +2,18 @@ class Player
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
+
+    def say_hello
+      "I'm #{@name} with a health of #{@health}."
+    end
   end
 end
 
 player1 = Player.new("moe")
-puts player1.inspect
+puts player1.say_hello
+
+player2 = Player.new("larry", 60)
+puts player2.say_hello
+
+player3 = Player.new("curly", 125)
+puts player3.say_hello
