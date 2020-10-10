d13a499ea63ec30ab9b4e1a49e337ff419069f42
Waihon Yew <yewwaihon@gmail.com>
2020-10-10 20:10:46 +0800

    Add players

--- studio_game.rb
+++ studio_game.rb
@@ -35,49 +35,35 @@ class Game
 
   def initialize(title)
     @title = title
+    @players = []
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  def play
+    puts "There are #{@players.size} players in #{@title}:"
+    @players.each do |player|
+      puts player
+    end
+    @players.each do |player|
+      player.blam
+      player.w00t
+      player.w00t
+      puts player
+    end
   end
 end
 
-player1 = Player.new("moe")
-puts player1
-player1.blam
-puts player1
+if __FILE__ == $0
+  player1 = Player.new("moe")
+  player2 = Player.new("larry", 60)
+  player3 = Player.new("curly", 125)
 
-player2 = Player.new("larry", 60)
-player2.w00t
-puts player2
-
-player3 = Player.new("curly", 125)
-puts player3
-player3.blam
-puts player3
-player3.w00t
-puts player3
-
-player2.name = "lawrence"
-puts player2.name
-
-players = [player1, player2, player3]
-puts "There are #{players.size} players in the game:"
-players.each do |player|
-  puts player
-end
-
-players.each do |player|
-  puts player.health
-end
-
-players.each do |player|
-  player.blam
-  player.w00t
-  player.w00t
-  puts player
-end
-
-players.pop
-player4 = Player.new("shemp", 90)
-players.push(player4)
-
-players.each do |player|
-  puts player
+  knuckleheads = Game.new("Knuckleheads")
+  knuckleheads.add_player(player1)
+  knuckleheads.add_player(player2)
+  knuckleheads.add_player(player3)
+  knuckleheads.play
 end
