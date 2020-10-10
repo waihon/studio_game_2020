d95ddcdea01b733b0c348ffb929644fae5a03b36
Waihon Yew <yewwaihon@gmail.com>
2020-10-10 16:37:58 +0800

    Create more blocks

--- studio_game.rb
+++ studio_game.rb
@@ -46,15 +46,30 @@ puts player3
 player3.w00t
 puts player3
 
-puts player1.health
-puts player2.health
-puts player3.health
-
 player2.name = "lawrence"
 puts player2.name
 
 players = [player1, player2, player3]
 puts "There are #{players.size} players in the game:"
+players.each do |player|
+  puts player
+end
+
+players.each do |player|
+  puts player.health
+end
+
+players.each do |player|
+  player.blam
+  player.w00t
+  player.w00t
+  puts player
+end
+
+players.pop
+player4 = Player.new("shemp", 90)
+players.push(player4)
+
 players.each do |player|
   puts player
 end
