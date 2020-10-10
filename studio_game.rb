3a8c71137f086fa319421fb1c804212a256b0855
Waihon Yew <yewwaihon@gmail.com>
2020-10-10 12:14:20 +0800

    Print the object

--- studio_game.rb
+++ studio_game.rb
@@ -3,7 +3,7 @@ class Player
     @name = name.capitalize
     @health = health
 
-    def say_hello
+    def to_s
       "I'm #{@name} with a health of #{@health}."
     end
 
@@ -20,18 +20,17 @@ class Player
 end
 
 player1 = Player.new("moe")
-puts player1.say_hello
+puts player1
 player1.blam
-puts player1.say_hello
+puts player1
 
 player2 = Player.new("larry", 60)
-puts player2.say_hello
 player2.w00t
-puts player2.say_hello
+puts player2
 
 player3 = Player.new("curly", 125)
-puts player3.say_hello
+puts player3
 player3.blam
-puts player3.say_hello
+puts player3
 player3.w00t
-puts player3.say_hello
+puts player3
