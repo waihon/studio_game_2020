53f8d5307006a6a4ea3d2ce22422cf4cfa395eec
Waihon Yew <yewwaihon@gmail.com>
2020-10-10 11:59:01 +0800

    Add state-changing behavior

--- studio_game.rb
+++ studio_game.rb
@@ -6,14 +6,32 @@ class Player
     def say_hello
       "I'm #{@name} with a health of #{@health}."
     end
+
+    def blam
+      @health -= 10
+      puts "#{@name} got blammed!"
+    end
+
+    def w00t
+      @health += 15
+      puts "#{@name} got w00ted!"
+    end
   end
 end
 
 player1 = Player.new("moe")
 puts player1.say_hello
+player1.blam
+puts player1.say_hello
 
 player2 = Player.new("larry", 60)
 puts player2.say_hello
+player2.w00t
+puts player2.say_hello
 
 player3 = Player.new("curly", 125)
+puts player3.say_hello
+player3.blam
+puts player3.say_hello
+player3.w00t
 puts player3.say_hello
