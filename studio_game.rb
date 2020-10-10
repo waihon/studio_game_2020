25eee66c7de9c927ed147e130f3f4e882334c770
Waihon Yew <yewwaihon@gmail.com>
2020-10-10 11:40:46 +0800

    Initialize the object's state

--- studio_game.rb
+++ studio_game.rb
@@ -1,4 +1,9 @@
 class Player
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
 end
 
-player1 = Player.new
+player1 = Player.new("moe")
+puts player1.inspect
