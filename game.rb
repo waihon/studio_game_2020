ba8a28139a85dea008fdf97b3789815a9eca64d7
Waihon Yew <yewwaihon@gmail.com>
2020-10-11 22:56:24 +0800

    Create a Game source file

---
+++ game.rb
@@ -0,0 +1,39 @@
+require_relative 'player'
+
+class Game
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @players = []
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  def play
+    puts "There are #{@players.size} players in #{@title}:"
+    @players.each do |player|
+      puts player
+    end
+    @players.each do |player|
+      player.blam
+      player.w00t
+      player.w00t
+      puts player
+    end
+  end
+end
+
+if __FILE__ == $0
+  player1 = Player.new("moe")
+  player2 = Player.new("larry", 60)
+  player3 = Player.new("curly", 125)
+
+  knuckleheads = Game.new("Knuckleheads")
+  knuckleheads.add_player(player1)
+  knuckleheads.add_player(player2)
+  knuckleheads.add_player(player3)
+  knuckleheads.play
+end
--- studio_game.rb
+++ studio_game.rb
@@ -1,29 +1,3 @@
-class Game
-  attr_reader :title
-
-  def initialize(title)
-    @title = title
-    @players = []
-  end
-
-  def add_player(player)
-    @players << player
-  end
-
-  def play
-    puts "There are #{@players.size} players in #{@title}:"
-    @players.each do |player|
-      puts player
-    end
-    @players.each do |player|
-      player.blam
-      player.w00t
-      player.w00t
-      puts player
-    end
-  end
-end
-
 if __FILE__ == $0
   player1 = Player.new("moe")
   player2 = Player.new("larry", 60)
