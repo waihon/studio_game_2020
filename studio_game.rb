e052b3f8c0305bb211289f02fba780e68671a530
Waihon Yew <yewwaihon@gmail.com>
2020-10-11 23:01:10 +0800

    Update the main program

--- studio_game.rb
+++ studio_game.rb
@@ -1,20 +1,21 @@
-if __FILE__ == $0
-  player1 = Player.new("moe")
-  player2 = Player.new("larry", 60)
-  player3 = Player.new("curly", 125)
+require_relative 'player'
+require_relative 'game'
 
-  knuckleheads = Game.new("Knuckleheads")
-  knuckleheads.add_player(player1)
-  knuckleheads.add_player(player2)
-  knuckleheads.add_player(player3)
-  knuckleheads.play
+player1 = Player.new("moe")
+player2 = Player.new("larry", 60)
+player3 = Player.new("curly", 125)
 
-  player4 = Player.new("alvin", 120)
-  player5 = Player.new("simon", 85)
-  player6 = Player.new("theodore", 105)
-  chipmunks = Game.new("Chipmunks")
-  chipmunks.add_player(player4)
-  chipmunks.add_player(player5)
-  chipmunks.add_player(player6)
-  chipmunks.play
-end
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
+
+player4 = Player.new("alvin", 120)
+player5 = Player.new("simon", 85)
+player6 = Player.new("theodore", 105)
+chipmunks = Game.new("Chipmunks")
+chipmunks.add_player(player4)
+chipmunks.add_player(player5)
+chipmunks.add_player(player6)
+chipmunks.play
