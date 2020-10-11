e7a3e0d3b3fc484bc836dd40ef1a41abec526134
Waihon Yew <yewwaihon@gmail.com>
2020-10-11 22:49:07 +0800

    Create a Player source file

--- studio_game.rb
+++ studio_game.rb
@@ -1,35 +1,3 @@
-class Player
-  attr_reader :health
-  attr_accessor :name
-
-  def initialize(name, health=100)
-    @name = name.capitalize
-    @health = health
-  end
-
-  def to_s
-    "I'm #{@name} with a health of #{@health} and a score of #{score}."
-  end
-
-  def blam
-    @health -= 10
-    puts "#{@name} got blammed!"
-  end
-
-  def w00t
-    @health += 15
-    puts "#{@name} got w00ted!"
-  end
-
-  def score
-    @health + @name.length
-  end
-
-  def name=(new_name)
-    @name = new_name.capitalize
-  end
-end
-
 class Game
   attr_reader :title
 
