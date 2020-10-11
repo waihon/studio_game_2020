8e3682f45b027d044f44dfbb5e4c9ed48b971dcc
Waihon Yew <yewwaihon@gmail.com>
2020-10-11 21:57:42 +0800

    Move other methods outside of initialize

--- studio_game.rb
+++ studio_game.rb
@@ -5,28 +5,28 @@ class Player
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
+  end
 
-    def to_s
-      "I'm #{@name} with a health of #{@health} and a score of #{score}."
-    end
+  def to_s
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
+  end
 
-    def blam
-      @health -= 10
-      puts "#{@name} got blammed!"
-    end
+  def blam
+    @health -= 10
+    puts "#{@name} got blammed!"
+  end
 
-    def w00t
-      @health += 15
-      puts "#{@name} got w00ted!"
-    end
+  def w00t
+    @health += 15
+    puts "#{@name} got w00ted!"
+  end
 
-    def score
-      @health + @name.length
-    end
+  def score
+    @health + @name.length
+  end
 
-    def name=(new_name)
-      @name = new_name.capitalize
-    end
+  def name=(new_name)
+    @name = new_name.capitalize
   end
 end
 
